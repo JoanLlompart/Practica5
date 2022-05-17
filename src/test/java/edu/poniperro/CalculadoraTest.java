2d1118edc8f9b2a241875b8466a90732f9473d08
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2022-05-17 19:34:23 +0200

    Segundo commit:test con JUNIT

---
+++ src/test/java/edu/poniperro/CalculadoraTest.java
@@ -0,0 +1,22 @@
+package edu.poniperro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void sumar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dividir() {
+    }
+}
