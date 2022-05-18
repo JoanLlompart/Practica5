82c85737f9bb9044b47fbc21f2c7dff3f91e1ccc
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2022-05-18 12:05:31 +0200

    Segundo commit:test con JUNIT con SETUP/BEFORE

--- src/test/java/edu/poniperro/CalculadoraTest.java
+++ src/test/java/edu/poniperro/CalculadoraTest.java
@@ -1,5 +1,9 @@
 package edu.poniperro;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
@@ -18,5 +22,29 @@ class CalculadoraTest {
 
     @org.junit.jupiter.api.Test
     void dividir() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testSumar() {
+    }
+
+    @Test
+    void testRestar() {
+    }
+
+    @Test
+    void testMultiplicar() {
+    }
+
+    @Test
+    void testDividir() {
     }
 }
